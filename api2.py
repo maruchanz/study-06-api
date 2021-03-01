f0b5a9f58f027c305b49a7a0a8ae77d4a628e8c8
maruchanz <62419423+maruchanz@users.noreply.github.com>
2021-03-02 08:10:21 +0900

    Add files via upload

--- api2.py
+++ api2.py
@@ -9,29 +9,23 @@ def get_api(url):
 
 
 def main():
-    # productId = ""
-    genreId = "503054"
-    url = "https://app.rakuten.co.jp/services/api/Product/Search/20170426?format=json&genreId={}&applicationId=1019079537947262807&maxPrice".format(
-        genreId)
+
+    productId = "c79426c3c98e443eefbfc26cfde6298f"
+    url = "https://app.rakuten.co.jp/services/api/Product/Search/20170426?format=json&productId={}&applicationId=1019079537947262807".format(
+        productId)
 
     result = get_api(url)
     # print(result)
     item_key = ['itemName', 'ProductDetails','maxPrice','minPrice']
     item_list = []
-
-    for i in range(len(result)):
-        tmp_item = {}
-        item = result['Products'][i]['Product']
-        for key, value in item.items():
-            if key in item_key:
-                tmp_item[key] =value
-        item_list.append(tmp_item)
-        print(item_list)
-    
-
-
-
-
+    # for i in range(len(result)):
+    tmp_item = {}
+    item = result['Products'][0]['Product']
+    for key, value in item.items():
+        if key in item_key:
+            tmp_item[key] =value
+    item_list.append(tmp_item)
+    print(item_list)
 
     # for name, price in zip(data['itemName'], data['itemPrice']):
     #     print(name + ':' + price)
--- api3.py
+++ api3.py
@@ -1,6 +1,6 @@
 import requests
 import urllib
-
+import pandas as pd
 
 def get_api(url):
     result = requests.get(url)
@@ -10,29 +10,36 @@ def get_api(url):
 
 def main():
     genreId = "503054"
-    url = "https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20170628?format=json&genreId={}&applicationId=1019079537947262807&maxPrice".format(
+    url = "https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20170628?format=json&genreId={}&applicationId=1019079537947262807".format(
         genreId)
 
     result = get_api(url)
-    print(result)
-    # item_key = ['itemName', 'itemPrice','maxPrice','minPrice']
-    # item_list = []
+    # print(result)
+    # df = pd.DataFrame({'itemName':[],'itemPrice':[],'rank':[]})
+    item_key = ['itemName', 'itemPrice','rank']
+    item_list = []
+    count = len(result['Items'])
+    # a = result['Items'][29]['Item']
+    # print(a)
 
-    # for i in range(len(result)):
-    #     tmp_item = {}
-    #     item = result['Items'][i]['Item']
-    #     for key, value in item.items():
-    #         if key in item_key:
-    #             tmp_item[key] =value
-    #     item_list.append(tmp_item)
-    #     print(item_list)
-    
+    for i in range(count):
+        tmp_item = {}
+        # APIで取得するサイトの構造に従って抽出
+        item = result['Items'][i]['Item']
+        # print(item)
+
+        for key, value in item.items():
+            if key in item_key:
+                tmp_item[key] = value
+        
+        item_list.append(tmp_item)
+    # print(item_list)
+   
+    df = pd.json_normalize(item_list)
+    df.to_csv('rank_list.csv') 
 
 
 
 
-
-    # for name, price in zip(data['itemName'], data['itemPrice']):
-    #     print(name + ':' + price)
 
 main()
